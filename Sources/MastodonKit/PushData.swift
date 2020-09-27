d7c800650335a058ba70a69f58c4233d8b324e37
Daniel Nitsikopoulos <daniel@dnitza.com>
2020-09-27 14:01:57 +1000

    Push notifications

--- Sources/MastodonKit/PushData.swift
+++ Sources/MastodonKit/PushData.swift
@@ -2,7 +2,36 @@
 //  PushData.swift
 //  MastodonKit
 //
-//  Created by Daniel Nitsikopoulos on 27/9/20.
+//  Created by Daniel Nitsikopoulos on 8/11/19.
 //
 
 import Foundation
+
+public struct PushData: Codable {
+    let alert: PushAlerts
+
+    public init(alert: PushAlerts) {
+        self.alert = alert
+    }
+}
+
+public struct PushAlerts: Codable {
+    /// (optional): Boolean of whether you want to receive follow notification event.
+    let follow: Bool?
+    /// (optional): Boolean of whether you want to receive favourite notification event.
+    let favourite: Bool?
+    /// (optional): Boolean of whether you want to receive reblog notification event.
+    let reblog: Bool?
+    /// (optional): Boolean of whether you want to receive mention notification event.
+    let mention: Bool?
+    /// (optional): Boolean of whether you want to receive poll result notification event.
+    let poll: Bool?
+
+    public init(follow: Bool?, favourite: Bool?, reblog: Bool?, mention: Bool?, poll: Bool?) {
+        self.follow = follow
+        self.favourite = favourite
+        self.reblog = reblog
+        self.mention = mention
+        self.poll = poll
+    }
+}
